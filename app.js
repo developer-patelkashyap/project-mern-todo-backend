93b76276dd6596667b90aef052b1cc921b4d170b
Kashyap Sureshchandra Patel <kashyappatel.developer@gmail.com>
2024-07-17 22:56:53 +0530

    Updated app.js, Added new dependencies and deleted .env

--- app.js
+++ app.js
@@ -1,11 +1,31 @@
+// core dependencies
 const express = require("express");
 require("dotenv").config();
-const connectDB = require("./config/db");
+const morgan = require("morgan");
 
+// server instance
 const app = express();
+
+// env variable
 const PORT = process.env.PORT || 9000;
 
 app.use(express.json());
+
+// DB connection
+const connectDB = require("./config/db");
+
 connectDB();
+
+// middleware
+const authMiddleware = require("./middlewares/auth");
+
+app.use(morgan("combined"));
+
+// routes
+const userRouter = require("./routes/User");
+const todoRouter = require("./routes/Todo");
+
+app.use("/api/v1/user", userRouter);
+app.use("/api/v1/todo", authMiddleware, todoRouter);
 
 app.listen(PORT, console.log(`Server running on PORT: ${PORT}`));
